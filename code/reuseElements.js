4ae4218049f37cf0e9ef9c9d92fc03e4aa560898
grigtod <licenses@projectionartworks.com>
2020-10-01 12:52:28 +0100

    added all pages

---
+++ code/reuseElements.js
@@ -0,0 +1,4 @@
+$(function () { $('#all_navbar').load('pages/all/all_navbar.html');});
+$(function () {	$('#all_topGradient').load('pages/all/all_topGradient.html');});
+$(function () { $('#all_bottomGradient').load('pages/all/all_bottomGradient.html');});
+$(function () { $('#all_divider').load('pages/all/all_divider.html');});
